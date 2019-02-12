c660dbf88b735ee7a688459ee78eeb0bb03b2554
Calvin Evans <calvin@webantic.co.uk>
2019-02-12 07:16:23 +0000

    reformat /register response

--- services/http.js
+++ services/http.js
@@ -48,8 +48,9 @@ export default (app, controller) => {
                 const id = uuid()
                 controller.storage.users.save({ id })
                 // return without waiting - should be fine
-                res.end(id)
+                res.json({ success: true, id })
             } catch (err) {
+                res.status(500).json({ success: false, message: err })
                 error(err)
             }
         }
