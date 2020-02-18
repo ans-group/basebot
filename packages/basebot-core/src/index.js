e30035ba42b30d5870ebb880b69f5d29e5388fc2
ceuk <1681236+ceuk@users.noreply.github.com>
2020-02-18 21:23:21 +0000

    bug fixes

--- packages/basebot-core/src/index.js
+++ packages/basebot-core/src/index.js
@@ -8,7 +8,7 @@ export const init = ({ skills, config }) => {
   const channels = getAllModules(config.channels)
   const rawMiddleware = getAllModules(config.middleware)
   const models = getAllModels(rawMiddleware)
-  const storage = getSingleModule(config.storage)({ logger, models: models && Object.assign(...models) })
+  const storage = getSingleModule(config.storage)({ logger, models: models && Array.isArray(models) ? Object.assign(...models) : models })
   const middleware = rawMiddleware.map(mw => mw({ storage, logger }))
   const info = logger('core', 'info')
   const { server, app } = Server({ logger })
