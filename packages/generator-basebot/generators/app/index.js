be7043081eb73c9695982c7b53bd5c5c5b04e00a
ceuk <1681236+ceuk@users.noreply.github.com>
2020-02-18 21:49:07 +0000

    only beautify js files when scaffolding new project

--- packages/generator-basebot/generators/app/index.js
+++ packages/generator-basebot/generators/app/index.js
@@ -1,5 +1,6 @@
 const path = require('path')
-var beautify = require('gulp-beautify')
+const beautify = require('gulp-beautify')
+const gulpif = require('gulp-if')
 const jetpack = require('fs-jetpack')
 const Generator = require('yeoman-generator')
 const coreDeps = require('./coredeps')
@@ -93,7 +94,8 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.registerTransformStream(beautify({ indent_size: 2 }))
+    const condition = file => file.extname === '.js'
+    this.registerTransformStream(gulpif(condition, beautify({ indent_size: 2 })))
     // Extend or create package.json file in destination path
     this.fs.extendJSON(this.destinationPath('package.json'), {
       'name': this.answers.projectName,
